a4a8a3d5cfba3061ca9517f53afa9de304c7d4e6
Camila <camila.fuentes.g@ug.uchile.cl>
2025-12-14 17:19:08 -0300

    Added task admin

--- backend/task_manager/admin.py
+++ backend/task_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user__username', 'status', 'created_at')
+    search_fields = ('title', 'user__username')
+    list_filter = ('status',)
